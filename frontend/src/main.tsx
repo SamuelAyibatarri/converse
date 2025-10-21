eec7e6752bd7b6c9a6c0f24053adafab6b59af7c
Samuel Ayibatarri <107348130+SamuelAyibatarri@users.noreply.github.com>
2025-10-21 21:28:21 +0100

    Add conditional navigation for user authentication

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Dashboard from './pages/Dashboard.tsx'
 import SignUpLogin from './pages/Signup-Login.tsx'
+
+const userData = JSON.parse(localStorage.getItem("user_data") as string);
 
 const router = createBrowserRouter([{
   path: '/auth',
-  element: <SignUpLogin />
+  element: (userData.isLoggedIn === true) ? <Navigate to={'/dashboard'} replace /> : <SignUpLogin />
 },
 {
   path: '/',
@@ -15,7 +18,12 @@ const router = createBrowserRouter([{
 },
 { path: '/chat',
   element: <App />
-}]);
+},
+{ 
+  path: '/dashboard',
+  element: (userData.isLoggedIn === true) ? <Dashboard /> : <Navigate to={'/auth'} replace />
+}
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
