68c12d8c2ae48cfdda2d56264b05283e2cf5093f
Samuel Ayibatarri <107348130+SamuelAyibatarri@users.noreply.github.com>
2025-10-21 21:27:41 +0100

    Integrate Toaster and modify Button usage
    Added Toaster component and commented out Button.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import ChatComponent  from "@/components/chat-component" ;
+import { sendMessage } from "./server";
+import { Button } from "./components/ui/button";
+import { Toaster }  from "@/components/ui/sonner"
 function App() {
 
   return (
     <>
     <div className="max-h-full">
       <ChatComponent />
-      <div id="test"></div>
-      <div id="test"></div>
+      <Toaster />
+      {/* <Button onClick={sendMessage}>Click me</Button> */}
     </div>
     </>
   )
