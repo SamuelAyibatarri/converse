cbc8762a243553f3cd11b2e95865524d86e5d4d5
Samuel Ayibatarri <107348130+SamuelAyibatarri@users.noreply.github.com>
2025-10-25 15:18:20 +0100

    Add optional name field to Chat interface

--- backend/src/Interfaces.ts
+++ backend/src/Interfaces.ts
@@ -19,6 +19,7 @@ export interface CustomerData {
 }
 
 export interface Chat {
+    name?: string;
     readonly timestamp: number;
     readonly content: string;
     readonly senderId: string;
@@ -27,17 +28,20 @@ export interface Chat {
 
 export interface ChatData {
     chatDataId: string;
+    senderId: string;
+    receiverId: string;
+    chatTimestamp: number;
     chatData: Chat[];
 }
 
-export interface CAI { // CAI -> Create Account Interface
+export interface CAI { /// CAI -> Create Account Interface
     name: string;
     email: string;
     passwordHash: string;
     usertype: "agent" | "customer";
 }
 
-export interface LAI { // LAI -> Login Account Interface
+export interface LAI { /// LAI -> Login Account Interface
     email: string;
     passwordHash: string;
     usertype: "agent" | "customer";
