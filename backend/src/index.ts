4dc86477e4699fe69714c6171bb7e8404b0ff933
Samuel Ayibatarri <107348130+SamuelAyibatarri@users.noreply.github.com>
2025-10-25 15:19:37 +0100

    Refactor comments and add chat functionality
    Updated comments for clarity and consistency throughout the code. Added new endpoints for creating chat data and sending messages.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ import { fileURLToPath } from 'url';
 import { stream, streamText, streamSSE } from 'hono/streaming'
 import { zstdCompress } from 'zlib';
 
-// recreate __dirname for ESM
+/// recreate __dirname for ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -31,9 +31,9 @@ let DBDATA: Interfaces.Database = {
   customers: [],
   chatData: []
 }
-// :::::::::::::::::::: Helper Functions :::::::::::::::
+/// :::::::::::::::::::: Helper Functions :::::::::::::::
 
-// load Database
+/// load Database
 function loadDb(): void {
   const _: Interfaces.Database = {
     agents: [],
@@ -65,7 +65,7 @@ function checkIfUserExists(formData: Interfaces.LAI | Interfaces.CAI): boolean {
   return false;
 }
 
-// Create a new agent 
+/// Create a new agent 
 function createAgent(_:Interfaces.CAI) {
   loadDb();
   if (checkIfUserExists(_)) {
@@ -87,7 +87,7 @@ function createAgent(_:Interfaces.CAI) {
   return data;
 }
 
-// Create Customer
+/// Create Customer
 function createCustomer(_:Interfaces.CAI) {
   if (checkIfUserExists(_)) {
     throw new Error("User already exists, try logging in instead");
@@ -110,7 +110,69 @@ function createCustomer(_:Interfaces.CAI) {
   return data;
 } 
 
-// Check login session
+/// Find user type from id
+function findUserType(userId: string): "agent" | "customer" | null {
+  loadDb();
+  if (DBDATA.agents.some(agent => agent.id === userId)) {
+    return "agent";
+  }
+  if (DBDATA.customers.some(customer => customer.id === userId)) {
+    return "customer";
+  }
+  return null;
+}
+
+// Create chat data, I don't even get the point of comments I believe my function's name convey their functionality pretty well
+function createChatData(_: Interfaces.ChatData) {
+  try {
+    loadDb();
+    let senderType: "agent" | "customer" | null= findUserType(_.senderId);
+    let receiverType: "agent" | "customer" | null = findUserType(_.receiverId);
+    if (senderType == null || receiverType == null) {
+      throw new Error("User type can't be null");
+    }
+    let sender, receiver;
+    if (senderType === "agent") {
+        sender = DBDATA.agents.find(x => x.id === _.senderId);
+    } else if(senderType === "customer") {
+      sender = DBDATA.customers.find(x => x.id === _.senderId);
+    }
+    if (receiverType === "agent") {
+        receiver = DBDATA.agents.find(x => x.id === _.receiverId);
+    } else if(senderType === "customer") {
+      receiver = DBDATA.customers.find(x => x.id === _.receiverId);
+    }
+    
+    if (sender) {
+      sender.chatHistory.push(_.chatDataId);
+    } else {
+      throw new Error("User not found");
+    }
+
+    if (receiver) {
+      receiver.chatHistory.push(_.chatDataId);
+    } else {
+      throw new Error("User not found");
+    }
+    DBDATA.chatData.push(_);   
+    fs.writeFileSync(dbPath, JSON.stringify(DBDATA, null, 2));
+  } catch (error) {
+    throw error;
+  }
+} 
+
+/// Update chat data
+function updateChatData(chatId: string, chat: Interfaces.Chat) {
+  loadDb();
+  const filteredChatData = DBDATA.chatData.find(x => x.chatDataId === chatId);
+  if (!filteredChatData) {
+    throw new Error("Chat data doesn't exist");
+  }
+  filteredChatData.chatData.push(chat);
+  fs.writeFileSync(dbPath, JSON.stringify(DBDATA, null, 2));
+}
+
+/// Check login session
 function checkLoginSession(_:Interfaces.LAI): boolean {
   loadDb();
   const currentTimestamp: number = Date.now()
@@ -124,7 +186,7 @@ function checkLoginSession(_:Interfaces.LAI): boolean {
   return true;
 }
 
-// Verify User
+/// Verify User
 function verifyUser(_:Interfaces.LAI): boolean {
   loadDb();
   const currentTimestamp: number = Date.now()
@@ -132,7 +194,7 @@ function verifyUser(_:Interfaces.LAI): boolean {
   if (_.usertype === "agent") loadedDbArr = DBDATA.agents;
   if (_.usertype === "customer") loadedDbArr = DBDATA.customers;
   let userData = loadedDbArr.filter( x => x.email === _.email && x.passwordHash === _.passwordHash );
-  if (!userData) return false;
+  if (userData.length === 0) return false;
   if (userData.length > 1) return false;
   const updatedData = DBDATA;
   for (let entry of updatedData[`${_.usertype}s`]) {
@@ -144,21 +206,33 @@ function verifyUser(_:Interfaces.LAI): boolean {
   return true;
 }
 
-// load User Data
+/// Verify User Id
+function verifyUserId(userId: string, userType: "agent" | "customer"): boolean {
+  loadDb();
+  let loadedDbArr: Interfaces.AgentData[] | Interfaces.CustomerData[] = [];
+  if (userType === "agent") loadedDbArr = DBDATA.agents;
+  if (userType === "customer") loadedDbArr = DBDATA.customers;
+  let userData = loadedDbArr.filter(_ => _.id === userId);
+  if (userData.length === 0) return false;
+  if (userData.length > 1) return false;
+  return true;
+}
+
+/// load User Data
 function loadUserData(_:Interfaces.LAI, verified: boolean) {
   if (!verified) {
     throw new Error("Verification false, can't load data");
   }
   loadDb();
   let userData = DBDATA[`${_.usertype}s`].filter(x => x.passwordHash === _.passwordHash && x.email === _.email)
-  // userData.map((x) => {
-  //   let emptyChatData:string[] = [];
-  //   x.chatHistory = emptyChatData;
-  // })
-  return JSON.stringify(userData)
+  /// userData.map((x) => {
+  ///   let emptyChatData:string[] = [];
+  ///   x.chatHistory = emptyChatData;
+  /// })
+  return JSON.stringify(userData[0])
 }
 
-// Verify LAI interface
+/// Verify LAI interface
 function isLAI(obj: any): obj is Interfaces.LAI {
   return (
     obj &&
@@ -168,7 +242,7 @@ function isLAI(obj: any): obj is Interfaces.LAI {
   )
 }
 
-// Verify CAI interface
+/// Verify CAI interface
 function isCAI(obj: any): obj is Interfaces.CAI {
   return (
     obj &&
@@ -180,20 +254,24 @@ function isCAI(obj: any): obj is Interfaces.CAI {
 }
 
 
-// Verify Auth Form Data
+/// Verify Auth Form Data
 function verifyAuthFormData(_: any, action: "login" | "signup"): 1000 | 1001 | 2000 | 2001 | 3000 {
   if (action === "login") {
-    if (!isLAI(_)) return 1001 //"Invalid login payload";
-    return 1000 //"Valid login payload";
+    if (!isLAI(_)) return 1001 ///"Invalid login payload";
+    return 1000 ///"Valid login payload";
   }
   if (action === "signup") {
-    if (!isCAI(_)) return 2001 //"Invalid signup payload";
-    return 2000 //"Valid signup payload"
+    if (!isCAI(_)) return 2001 ///"Invalid signup payload";
+    return 2000 ///"Valid signup payload"
   }
-  return 3000 // Should never return this
+  return 3000 /// Should never return this
+}
+
+function createChat(senderId: string, receiverId: string) {
+
 }
  
-//:::::::::::: Endpoints ::::::::::::::::
+///:::::::::::: Endpoints ::::::::::::::::
 
 app.get('/', (c) => {
   return c.text('Hono API is running! 🚀')
@@ -220,13 +298,19 @@ app.post('/api/auth/login/:usertype', async (c) => {
   }
   const usertype: "agent" | "customer" = c.req.param('usertype') as "agent" | "customer";
   try {
-    if (verifyUser(formData)) return c.json({message: 'Login Successful!', userData: loadUserData(formData, verifyUser(formData))}, 200) 
+    const isUserVerified = verifyUser(formData);
+    if (!isUserVerified) { 
+      throw new Error("Invalid login details, check your email and password")
+    } else {
+      return c.json({message: 'Login Successful!', userData: loadUserData(formData, verifyUser(formData))}, 200) 
+    }
+
   } catch (error) {
     return c.json({ error: 'Failed to login', details: String(error) }, 500);
   }
 })
 
-// Create a new user
+/// Create a new user
 app.post('/api/auth/signup/:usertype', async (c) => {
   const formData: Interfaces.CAI = await c.req.json();
   const _ = verifyAuthFormData(formData, "signup");
@@ -241,7 +325,7 @@ app.post('/api/auth/signup/:usertype', async (c) => {
     case 3000:
       return c.json({error: "Invalid payload"}, 500);
     default: 
-      // Validation passed, do nothing and continue
+      /// Validation passed, do nothing and continue
       break;
   }
   
@@ -250,25 +334,80 @@ app.post('/api/auth/signup/:usertype', async (c) => {
   try {
       if (usertype === "agent") { 
         const user = createAgent(formData);
-        return c.json({message: `Agent ${formData.name} create successfully`, data: user}, 200)
+        return c.json({message: `Agent ${formData.name} create successfully`, userData: user}, 200)
       } 
       
       if (usertype === "customer") {
         const user = createCustomer(formData);
-        return c.json({message: `Customer ${formData.name} create successfully`, data: user}, 200)
+        return c.json({message: `Customer ${formData.name} create successfully`, userData: user}, 200)!
       } 
 
-      return c.json({ error: "Invalid user type specified in URL" }, 400); // 400 Bad Request
+      return c.json({ error: "Invalid user type specified in URL" }, 400); /// 400 Bad Request
 
   } catch (error) {
     const detailsMessage = (error instanceof Error) ? error.message : String(error);
     
     if (detailsMessage === "User already exists, try logging in instead") {
-      return c.json({ Error: 'Failed to create user', details: detailsMessage }, 409); // 409 Conflict
+      return c.json({ Error: 'Failed to create user', details: detailsMessage }, 409); /// 409 Conflict
     }
 
     return c.json({ Error: 'Failed to create user', details: detailsMessage }, 500);
   }
+})
+
+/// Create a chatData
+app.post('/api/createChat/:userType/:userId', async (c) => {
+    const formData: {receiverId: string, receiverType: "agent" | "customer"} = await c.req.json();
+    const userType: "agent" | "customer" = c.req.param('userType') as "agent" | "customer";
+    const userId: string = c.req.param('userId');
+    const isSenderVerified: boolean = verifyUserId(userId, userType);
+    const isReceiverVerified: boolean = verifyUserId(formData.receiverId, formData.receiverType);
+    if (isSenderVerified && isReceiverVerified) {
+      try {
+        const newChatData: Interfaces.ChatData = {
+          chatDataId: uuidv4(),
+          chatTimestamp: Date.now(),
+          senderId: userId,
+          receiverId: formData.receiverId,
+          chatData: []
+        }
+        createChatData(newChatData);
+        return c.json({message: 'Data created successfully', chatDataId: newChatData.chatDataId}, 201);
+      } catch (error) {
+       return c.json({message: 'An error occured', details: error}, 500); 
+      }
+    }
+
+})
+
+/// Send a message
+app.post('/api/chat/:chatId', async (c) => {
+    const chatId: string = c.req.param('chatId');
+    const formData: {
+        readonly content: string;
+        readonly senderId: string;
+        readonly receiverId: string;
+      } = await c.req.json();
+
+    const senderType: "agent" | "customer" | null = findUserType(formData.senderId);
+    const receiverType: "agent" | "customer" | null = findUserType(formData.receiverId);
+    if (senderType == null || receiverType == null) throw new Error("User type can't be null");
+
+    const _: Interfaces.Chat = {
+      timestamp: Date.now(),
+      content: formData.content,
+      senderId: formData.senderId,
+      receiverId: formData.receiverId
+    }
+    try {
+      if(verifyUserId(formData.senderId, senderType) && verifyUserId(formData.receiverId, receiverType)) {
+          updateChatData(chatId, _);
+          return c.json({message: "Message sent successfully", data: _}, 201)
+      }
+      throw new Error("User Id not valid")
+    } catch (error) {
+      return c.json({message: "An error occured when trying to send your message", details: error}, 500);
+    }
 })
 
 export default app
